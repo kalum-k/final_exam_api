ee844cef09102551a0cdd4e3f403b87bb61a7c59
Sirinrat <614259029@webmail.npru.ac.th>
2021-03-17 13:36:54 +0700

    succc

--- src/component/PhotoListFrom.js
+++ src/component/PhotoListFrom.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { Container } from "reactstrap";
+
 /*import { faHeart,faComment,faShare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";*/
 
 const UserProfileFrom = ({ username }) => {
 
     //key จาก unspash
-    const accessKey = "6rKVEZd1gWGlA8FLgveFQZFC7sOucq0rpGK9hqf1W-4"  
+    const accessKey = "6rKVEZd1gWGlA8FLgveFQZFC7sOucq0rpGK9hqf1W-4"
     //เรียก api ด้วย username 
     const apiuser = "https://api.unsplash.com/users/" + username + "/photos?page=1&query=&per_page=15&client_id=" + accessKey;
 
@@ -19,19 +21,21 @@ const UserProfileFrom = ({ username }) => {
         });
     }, [username])
     return (
-        <form>
-            <div className="row">
-                {pics.map(pics => (
-                    <div key={pics.id}>
-                        <section className="photo">
-                            <div className="card-list" tabindex="0">
-                                <img src={pics.urls.small} className="gallery-image" alt=""></img>
-                            </div>
-                        </section>
-                    </div >
-                ))}
-            </div>
-        </form >
+        <Container>
+            <form>
+                <div className="row">
+                    {pics.map(pics => (
+                        <div key={pics.id}>
+                            <section className="photo">
+                                <div className="card-list" tabindex="0">
+                                    <img src={pics.urls.small} className="gallery-image" alt=""></img>
+                                </div>
+                            </section>
+                        </div >
+                    ))}
+                </div>
+            </form >
+        </Container>
     );
 }
 export default UserProfileFrom;
